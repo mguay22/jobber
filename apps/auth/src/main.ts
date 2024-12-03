00a81159419c3d9f6e5b302862d27381a6f68719
Michael Guay <michaelguay@Michaels-Mac-Studio.local>
2024-12-02 19:24:43 -0500

    Add rest of deployments; grpc url env

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app/app.module';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { init } from '@jobber/nestjs';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -12,6 +13,7 @@ async function bootstrap() {
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
     options: {
+      url: app.get(ConfigService).getOrThrow('AUTH_GRPC_SERVICE_URL'),
       package: AUTH_PACKAGE_NAME,
       protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
     },

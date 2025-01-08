e15e3927b03291de99c668b77a9075def5b8158e
Michael Guay <guay.me@gmail.com>
2025-01-07 20:10:49 -0500

    Block if queue full

--- libs/pulsar/src/lib/pulsar.client.ts
+++ libs/pulsar/src/lib/pulsar.client.ts
@@ -15,6 +15,7 @@ export class PulsarClient implements OnModuleDestroy {
   async createProducer(topic: string) {
     const producer = await this.client.createProducer({
       topic,
+      blockIfQueueFull: true,
     });
     this.producers.push(producer);
     return producer;
